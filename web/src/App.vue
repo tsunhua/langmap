65f232f2e975b29c7e531a05d486309461f023a0
share.lin <linlshare@gmail.com>
2026-03-05 09:56:49 +0800

    暫時隱藏handbook入口

--- web/src/App.vue
+++ web/src/App.vue
@@ -204,13 +204,13 @@
                   </svg>
                   {{ $t('collections') || 'Collections' }}
                 </router-link>
-                <router-link to="/handbooks" class="flex items-center px-4 py-2 text-sm text-slate-700 hover:bg-slate-100"
+                <!-- <router-link to="/handbooks" class="flex items-center px-4 py-2 text-sm text-slate-700 hover:bg-slate-100"
                   role="menuitem" @click="resourcesDropdownOpen = false">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332 0.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5S19.832 5.477 21 6.253v13C19.832 18.477 18.246 18 16.5 18c-1.746 0-3.332 0.477-4.5 1.253" />
                   </svg>
                   {{ $t('handbook_title') }}
-                </router-link>
+                </router-link> -->
                 <router-link to="/languages" class="flex items-center px-4 py-2 text-sm text-slate-700 hover:bg-slate-100"
                   role="menuitem" @click="resourcesDropdownOpen = false">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
