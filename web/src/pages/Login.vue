38e40d2510f91be87d40e35b067d8246b7a9e26b
tsunhua <linlshare@gmail.com>
2026-03-06 20:57:05 +0800

    更新登錄頁面的表單位置

--- web/src/pages/Login.vue
+++ web/src/pages/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-slate-50 py-4 sm:py-12 px-4 sm:px-6 lg:px-8">
+  <div class="min-h-screen flex items-start justify-center pt-16 sm:pt-24 px-4 sm:px-6 lg:px-8 bg-slate-50">
     <div class="max-w-md w-full space-y-8">
       <div>
         <h2 class="mt-2 sm:mt-6 text-center text-3xl font-extrabold text-slate-900">
