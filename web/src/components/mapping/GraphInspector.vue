7af20ef8ca88b804823c98043f345ed734a1458b
tsunhua <lshare@outlook.com>
2026-08-19 23:57:55 +0800

    fix(mapping): avoid duplicate locale label

--- web/src/components/mapping/GraphInspector.vue
+++ web/src/components/mapping/GraphInspector.vue
@@ -77,7 +77,6 @@ const crossEdgeCount = computed(() => relatedCrossEdges.value.length)
 
     <div class="gi-meta">
       <span class="gi-lang">{{ node.lang_code }}</span>
-      <span v-if="node.language_profile_code" class="gi-lang-profile">{{ node.language_profile_code }}</span>
       <span v-if="node.language_name" class="gi-lang-name">{{ node.language_name }}</span>
       <span class="gi-depth">{{ t('components.depth', { depth: node.depth }) }}</span>
     </div>
